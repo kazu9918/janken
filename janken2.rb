6f7180fb7aa549417fc9aaa8458f15e85f428d3c
Kazuya <kazukinggolf@gmail.com>
2021-11-18 19:04:42 +0900

    引き分けの処理・HandTypeの表示の実装

--- janken2.rb
+++ janken2.rb
@@ -2,12 +2,16 @@ require_relative './program.rb'
 require_relative './man.rb'
 require_relative './judge.rb'
 
-player1 = Program.new
-player2 = Man.new
-print "最初はグー、じゃんけん...\n[0]:グー\n[1]:チョキ\n[2]:パー\n(0,1,2いずれかを入力してEnter)"
-hand1 = player1.show_hand
-hand2 = player2.show_hand
-judge = Judge.new(hand1, hand2)
-print "あなたの手:#{hand1}, プログラムの手:#{hand2}で\n"
-puts judge.result
-
+begin
+  player1 = Program.new
+  player2 = Man.new
+  print "最初はグー、じゃんけん...\n[0]:グー\n[1]:チョキ\n[2]:パー\n(0,1,2いずれかを入力してEnter)"
+  hand1 = player1.show_hand
+  begin
+    hand2 = player2.show_hand
+  end until hand2 == 0 or hand2 == 1 or hand2 == 2
+  judge = Judge.new(hand1, hand2)
+  Hand = ["グー", "チョキ", "パー"]
+  print "あなたの手:#{Hand[hand1]}, プログラムの手:#{Hand[hand2]}で\n"
+  puts judge.result
+end while hand1 == hand2
--- man.rb
+++ man.rb
@@ -2,7 +2,7 @@ class Man
   def show_hand
     begin
       n = gets.chomp
-    end until n == "0" or n == "1" or n == "2"
+    end
     n.to_i
   end
 end
