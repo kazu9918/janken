55f97778ad1d06fc060748a42014728741039471
Kazuya <kazukinggolf@gmail.com>
2021-11-26 16:50:12 +0900

    友人対戦文言の追加

--- man.rb
+++ man.rb
@@ -14,7 +14,7 @@ class Man
 
   def show_hand
     begin
-      print "最初はグー、じゃんけん...\n[0]:グー\n[1]:チョキ\n[2]:パー\n[3]:最強手\n(0,1,2,3いずれかを入力してEnter)"
+      print "最初はグー、じゃんけん...\n[0]:グー\n[1]:チョキ\n[2]:パー\n[3]:最強手\n#{@name}は(0,1,2,3いずれかを入力してEnter)"
       n = gets.chomp
     end until n == "0" or n == "1" or n == "2" or n == "3"
 
