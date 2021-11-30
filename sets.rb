506f2532952afb54036a6d89bb60b1e3b2b6bd95
Kazuya <kazukinggolf@gmail.com>
2021-11-30 15:25:11 +0900

    setsのリファクタ

--- sets.rb
+++ sets.rb
@@ -2,29 +2,35 @@ class Sets
   def initialize(player1, player2)
     @player1 = player1
     @player2 = player2
-    @player1count = 0
-    @player2count = 0
+    player1_win_count = 0
+    player2_win_count = 0
     @sets = []
   end
 
   def push(set)
     @sets << set
-    @player1count = @sets.count{|set| set.winner == @player1}
-    @player2count = @sets.count{|set| set.winner == @player2}
+  end
+
+  def player1_win_count
+    @sets.count{|set| set.winner == @player1}
+  end
+
+  def player2_win_count
+    @sets.count{|set| set.winner == @player2}
   end
 
   def print_count
     puts "第#{@sets.size + 1}セット"
-    puts "現在: #{@player1.name}#{@player1count}勝 #{@player2.name}#{@player2count}勝です"
+    puts "現在: #{@player1.name}#{player1_win_count}勝 #{@player2.name}#{player2_win_count}勝です"
   end
 
   def finished?
-    @player1count < 2 && @player2count < 2
+    player1_win_count < 2 && player2_win_count < 2
   end
 
   def print_result
-    print "最終結果： #{@player1.name}#{@player1count}勝 #{@player2.name}#{@player2count}勝で"
-    if @player1count > @player2count
+    print "最終結果： #{@player1.name}#{player1_win_count}勝 #{@player2.name}#{player2_win_count}勝で"
+    if player1_win_count > player2_win_count
       print "#{@player1.name}の勝ちです"
     else
       print "#{@player2.name}の勝ちです"
